154d1b1002a92bca357a26e319b4a6222f99809d
Jesús Castro <x51v4n@gmail.com>
2020-10-29 00:39:37 -0600

    Update deploy script
    Add signing flag.

--- deploy.sh
+++ deploy.sh
@@ -22,7 +22,6 @@ echo "Generating site"
 hugo
 
 echo "Updating gh-pages branch"
-cp ./CNAME public 
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+cd public && git add --all && git commit -m -S "Publishing to gh-pages (using deploy.sh)"
 
 git push origin gh-pages
